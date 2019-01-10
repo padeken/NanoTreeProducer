9186a37aff30a2bccec506f826fd61a59d0a6466
IzaakWN <izaak.neutelings@cern.ch>
2019-01-10 17:19:36 +0100

    debugged

--- resubmit.py
+++ resubmit.py
@@ -1,27 +1,28 @@
 #! /usr/bin/env python
 
-import os, glob, sys, shlex
+import os, glob, sys, shlex, re
 from commands import getoutput
 from argparse import ArgumentParser
-from checkFiles import getSampleShortName, matchSample
-from submit_qsub import bcolors, createJobs, getFileListPNFS, getFileListDAS, submitJobs, split_seq
+import checkFiles, submit_qsub
+from checkFiles import getSampleShortName, matchSampleToPattern
+from submit_qsub import args, bcolors, createJobs, getFileListPNFS, getFileListDAS, submitJobs, split_seq
 import itertools
 import subprocess
 from ROOT import TFile, Double
 
-parser = ArgumentParser()
-parser.add_argument('-c', '--channel', dest='channel', type=str, default="tautau", action="store")
-parser.add_argument('-n', '--njob',    dest='njob', type=int, default=10, action="store")
-#parser.add_argument('-m', '--make',    dest='make', default=False, action="store_true")
-parser.add_argument('-m', '--mock',    dest='mock', action='store_true', default=False,
-                                       help="mock submit jobs for debugging purposes" )
-parser.add_argument('-y', '--year',    dest='year', choices=[2017,2018], type=int, default=2017, action='store',
-                                       help="select year" )
-parser.add_argument('-s', '--samples', dest='samples', type=str, nargs='+', default=[ ], action="store",
-                                       help="samples to run over, glob patterns (wildcards * and ?) are allowed.")
-parser.add_argument('-v', '--verbose', dest='verbose', default=False, action='store_true',
-                                       help="set verbose" )
-args = parser.parse_args()
+# parser = ArgumentParser()
+# parser.add_argument('-c', '--channel', dest='channel', type=str, default="tautau", action="store")
+# parser.add_argument('-n', '--njob',    dest='njob', type=int, default=10, action="store")
+# #parser.add_argument('-m', '--make',    dest='make', default=False, action="store_true")
+# parser.add_argument('-m', '--mock',    dest='mock', action='store_true', default=False,
+#                                        help="mock submit jobs for debugging purposes" )
+# parser.add_argument('-y', '--year',    dest='year', choices=[2017,2018], type=int, default=2017, action='store',
+#                                        help="select year" )
+# parser.add_argument('-s', '--samples', dest='samples', type=str, nargs='+', default=[ ], action="store",
+#                                        help="samples to run over, glob patterns (wildcards * and ?) are allowed.")
+# parser.add_argument('-v', '--verbose', dest='verbose', default=False, action='store_true',
+#                                        help="set verbose" )
+# args = parser.parse_args()
 
 
 
@@ -31,14 +32,14 @@ def main():
     year        = args.year
     channel     = args.channel
     outdir      = "output_%s/"%(year)
-    os.chdir(outdir)
     chunkpattern = re.compile(r".*_(\d+)_[a-z]+\.root")
     
     # GET LIST
     samplelist = [ ]
-    for directory in sorted(os.listdir('./')):
-        if not os.path.isdir(directory): continue
-        if args.samples and not matchSampleToPattern(args.samples,directory): continue
+    for directory in sorted(os.listdir(outdir)):
+        if not os.path.isdir(outdir+directory): continue
+        if args.samples and not matchSampleToPattern(directory,args.samples): continue
+        if args.veto and not matchSampleToPattern(directory,args.veto): continue
         samplelist.append(directory)
     if not samplelist:
       print "No samples found in %s!"%(outdir)
@@ -48,7 +49,8 @@ def main():
     # RESUBMIT samples
     for directory in samplelist:
         #if directory.find('W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8__ytakahas-NanoTest_20180507_W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8-a7a5b67d3e3590e4899e147be08660be__USER')==-1: continue
-        outfilelist = glob.glob(directory + '/*_' + args.channel + '.root')
+        outdir = "output_%s/%s"%(year,directory)
+        outfilelist = glob.glob(outdir + '/*_' + args.channel + '.root')
         if not outfilelist: continue
         
         # GET INPUT FILES
@@ -57,7 +59,6 @@ def main():
         else:
           infiles = getFileListDAS('/' + directory.replace('__', '/'))
         
-        outdir = "output_%s/%s"%(year,directory)
         infilelists = list(split_seq(infiles, args.njob))
         
         badchunks   = [ ]
@@ -67,11 +68,16 @@ def main():
           for filename in outfilelist:
               match = chunkpattern.search(filename)
               if match:
-                chunk = int(match.group(0))
+                chunk = int(match.group(1))
               else:
                 print bcolors.BOLD + bcolors.FAIL + '[NG] did not recognize output file %s !'%(filename) + bcolors.ENDC
                 exit(1)
-              misschunks.remove(chunk)
+              if chunk in misschunks:
+                misschunks.remove(chunk)
+              elif chunk >= len(infilelists):
+                print bcolors.BOLD + bcolors.FAIL + '[WN] %s: found chunk %s >= total number of chunks %s ! Please make sure you have chosen the correct -n !'%(filename,chunk,len(infilelists)) + bcolors.ENDC
+              else:
+                print bcolors.BOLD + bcolors.FAIL + '[WN] %s: found weird chunk %s ! Please check if there is overcounting !'%(filename,chunk,len(infilelists)) + bcolors.ENDC
               file = TFile(filename,'READ')
               if not file.IsZombie() and file.GetListOfKeys().Contains('tree') and file.GetListOfKeys().Contains('cutflow'):
                 continue
@@ -81,13 +87,13 @@ def main():
           
           # BAD CHUNKS
           if len(badchunks)>0:
-            chunktext = ('chunks' if len(badchunks)>1 else 'chunk') + ', '.join(str(ch) for ch in badchunks)
+            chunktext = ('chunks ' if len(badchunks)>1 else 'chunk ') + ', '.join(str(ch) for ch in badchunks)
             print bcolors.BOLD + bcolors.WARNING + '[NG] %s, %d/%d failed! Resubmitting %s...'%(directory,len(ids),len(outfilelist),chunktext) + bcolors.ENDC
           
           # MISSING CHUNKS
           if len(misschunks)>0:
-            chunktext = ('chunks' if len(misschunks)>1 else 'chunk') + ', '.join(str(i) for i in misschunks)
-            print bcolors.BOLD + bcolors.WARNING + "[WN] %s missing %d/%d files ! Resubmitting %s...%s"%(directory,imax-len(outfilelist),len(outfilelist),chunktext) + bcolors.ENDC
+            chunktext = ('chunks ' if len(misschunks)>1 else 'chunk ') + ', '.join(str(i) for i in misschunks)
+            print bcolors.BOLD + bcolors.WARNING + "[WN] %s missing %d/%d files ! Resubmitting %s..."%(directory,len(misschunks),len(outfilelist),chunktext) + bcolors.ENDC
             for chunk in misschunks:
               infiles = infilelists[chunk]
               createJobs(jobslog,infiles,outdir,directory,chunk,channel,year=year)
