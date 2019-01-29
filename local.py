1356a9bb6bb95b40d2fad91a53823b1af6983fa3
IzaakWN <izaak.neutelings@cern.ch>
2019-01-29 11:23:39 +0100

    add more data files with fewer events; add tag option

--- local.py
+++ local.py
@@ -10,26 +10,30 @@ parser.add_argument('-c', '--channel', dest='channel', action='store', choices=[
 parser.add_argument('-t', '--type',    dest='type', action='store', choices=['data','mc'], default='mc')
 parser.add_argument('-y', '--year',    dest='year', action='store', choices=[2016,2017,2018], type=int, default=2017)
 parser.add_argument('-T', '--tes',     dest='tes', action='store', type=float, default=1.0)
+parser.add_argument('-l', '--tag',     dest='tag', action='store', type=str, default="")
+parser.add_argument('-Z', '--doZpt',   dest='doZpt', action='store_true', default=False)
 args = parser.parse_args()
 
 channel  = args.channel
 year     = args.year
 dataType = args.type
 infiles  = args.infiles
-postfix  = channel + '.root'
+if args.tag and args.tag[0]!='_': args.tag = '_'+args.tag
+postfix  = channel + args.tag + '.root'
 kwargs = {
   'year':  args.year,
   'tes':   args.tes,
+  'doZpt': args.doZpt,
 }
-
-if isinstance(infiles,str):
-  infiles = infiles.split(',')
 
 if channel=='etau':
   channel = 'eletau'
 elif channel=='elemu':
   channel = 'muele'
 
+
+if isinstance(infiles,str):
+  infiles = infiles.split(',')
 if infiles:
   dataType = 'mc'
   if infiles[0].find("/SingleMuon/")!=-1 or infiles[0].find("/Tau/")!=-1 or infiles[0].find("/SingleElectron/")!=-1:
@@ -37,7 +41,14 @@ if infiles:
 else:
   if dataType=='data':
     if year==2017:
-      infiles = ['root://cms-xrd-global.cern.ch//store/data/Run2017B/Tau/NANOAOD/31Mar2018-v1/10000/04463969-D044-E811-8DC1-0242AC130002.root' ]
+      if channel=='mutau':
+        infiles = [
+          'root://cms-xrd-global.cern.ch//store/data/Run2017C/SingleMuon/NANOAOD/31Mar2018-v1/100000/EC085F70-1A64-E811-A184-003048F2E8C2.root', #    4686
+          'root://cms-xrd-global.cern.ch//store/data/Run2017C/SingleMuon/NANOAOD/31Mar2018-v1/30000/3666FB57-B255-E811-BC5F-1866DA7F7AC2.root',  #   43637
+#           'root://cms-xrd-global.cern.ch//store/data/Run2017C/SingleMuon/NANOAOD/31Mar2018-v1/30000/DCA41AD0-A752-E811-A430-44A8420CC940.root',  # 1878250
+        ]
+      else:
+        infiles = ['root://cms-xrd-global.cern.ch//store/data/Run2017B/Tau/NANOAOD/31Mar2018-v1/10000/04463969-D044-E811-8DC1-0242AC130002.root' ]
     else:
       infiles = ['root://cms-xrd-global.cern.ch//store/group/phys_tau/ProdNanoAODv4Priv/16dec18/DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18NanoAODv4Priv-from_102X_upgrade2018_realistic_v15_ver2/181216_125027/0000/myNanoRunMc2018_NANO_75.root'
       ] 
@@ -48,12 +59,13 @@ else:
     if year==2017:
 #       infiles = [ 'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAOD/DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14_ext1-v1/10000/0A5AB04B-4B42-E811-AD7F-A4BF0112BDE6.root']
 #       infiles = [ 'dcap://t3se01.psi.ch:22125//pnfs/psi.ch/cms/trivcat/store/user/ytakahas/LQ3ToTauB_Fall2017_5f_Madgraph_LO_pair-M500/nanoAOD/v1/nanoAOD_LQ3ToTauB_Fall2017_5f_Madgraph_LO_pair-M500_1602.root']
-      infiles = [ #'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1/70000/B28E4243-3245-E811-B18F-001E67E71BAA.root', #  117636
-                  'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1/90000/54362765-8948-E811-9E4A-001F29085CDE.root', #   84396
-#                   'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1/70000/002CA305-5C44-E811-9310-E0071B73B6E0.root', # 1513969
-#                   'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1/20000/B82F7904-0547-E811-BBB6-44A842CF05CC.root', 
-#                   'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1/20000/A0397F3A-7A44-E811-843F-5065F3812271.root', 
-#                   'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1/90000/1CEBB44E-4548-E811-8798-A4BF0115951C.root',
+      infiles = [
+        #'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1/70000/B28E4243-3245-E811-B18F-001E67E71BAA.root', #  117636
+        'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1/90000/54362765-8948-E811-9E4A-001F29085CDE.root', #   84396
+        #'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1/70000/002CA305-5C44-E811-9310-E0071B73B6E0.root', # 1513969
+        #'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1/20000/B82F7904-0547-E811-BBB6-44A842CF05CC.root', 
+        #'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1/20000/A0397F3A-7A44-E811-843F-5065F3812271.root', 
+        #'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1/90000/1CEBB44E-4548-E811-8798-A4BF0115951C.root',
       ]
 #       infiles = [ 'dcap://t3se01.psi.ch:22125//pnfs/psi.ch/cms/trivcat/store/user/ytakahas/VectorLQ3ToTauB_Fall2017_5f_Madgraph_LO_pair_M1000/nanoAOD/v1/nanoAOD_VectorLQ3ToTauB_Fall2017_5f_Madgraph_LO_pair_M1000_1036.root',
 #                   'dcap://t3se01.psi.ch:22125//pnfs/psi.ch/cms/trivcat/store/user/ytakahas/VectorLQ3ToTauB_Fall2017_5f_Madgraph_LO_pair_M1000/nanoAOD/v1/nanoAOD_VectorLQ3ToTauB_Fall2017_5f_Madgraph_LO_pair_M1000_105.root',
