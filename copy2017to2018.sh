6d39615632c287cba3c348c1b5a1c45dcbd826ee
IzaakWN <izaak.neutelings@cern.ch>
2019-01-30 18:32:02 +0100

    remove new samples

--- copy2017to2018.sh
+++ copy2017to2018.sh
@@ -4,25 +4,26 @@ SCRATCH2017="/scratch/ineuteli/analysis/LQ_2017"
 SCRATCH2018="/scratch/ineuteli/analysis/LQ_2018"
 CHANNEL="tautau"
 SAMPLES="
-TT/TTToSemiLeptonic
-DY/DYJetsToLL_M-10to50
-DY/DY4JetsToLL_M-50
-WJ//WJetsToLNu
-WJ//W1JetsToLNu
-ST/ST_t-channel_top
-ST/ST_t-channel_antitop
-VV/WZ
+  #TT/TTToSemiLeptonic
+  DY/DYJetsToLL_M-10to50
+  DY/DY4JetsToLL_M-50
+  WJ//WJetsToLNu
+  ST/ST_t-channel_top
+  ST/ST_t-channel_antitop
+  #VV/WZ
 "
 
 for samplename in $SAMPLES; do
+  [[ $samplename = '#'* ]] && continue
+  echo $samplename
   
   filename1="$SCRATCH2017/${samplename}_${CHANNEL}.root"
   filename2="$SCRATCH2018/${samplename}_${CHANNEL}.root"  
 
   [ ! -e $filename ] && echo "Warning! $filename1 does not exist!" && continue 
-
-  #echo "Copying $filename1 to $filename2!"
-  cp -v $filename1 $filename2
+  
+  echo "Copying $filename1 to $filename2!"
+  #cp -v $filename1 $filename2
   
 done
 
