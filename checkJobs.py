73889f71cf1d3b0ba85fe19f1a93b2aa55219fd6
IzaakWN <izaak.neutelings@cern.ch>
2019-01-30 18:31:09 +0100

    add more statuses; add check of running jobs

--- checkJobs.py
+++ checkJobs.py
@@ -33,6 +33,8 @@ if __name__ == '__main__':
                                            help="veto this sample" )
     parser.add_argument('-t', '--type',    dest='type', choices=['data','mc'], type=str, default=None, action='store',
                                            help="filter data or MC to submit" )
+    parser.add_argument('-r', '--running', dest='running', default=False, action='store_true',
+                                           help="get running jobs from qstat" )
     parser.add_argument('-v', '--verbose', dest='verbose', default=False, action='store_true',
                                            help="set verbose" )
     args = parser.parse_args()
@@ -41,36 +43,49 @@ filepattern     = re.compile(r".*\.o(\d+)\.(\d+)")
 qstatpattern    = re.compile(r"(\d+) *\d\.\d+ *\w+ *\w+ *(\w+) *\d\d/\d\d/20\d\d *\d\d:\d\d:\d\d *(?:[^ ]+@[^ ]+)? *\d+ *(\d+(?:-\d+:\d)?)")
 nodepattern     = re.compile(r"Running job on machine .* (t3wn\d+\.psi\.ch)")
 queuepattern    = re.compile(r"(\w+\.q)@(t3wn\d+\.psi\.ch)")
-rootpattern     = re.compile(r"root://.*?/.*?\.root")
 RTWpattern      = "RuntimeWarning: creating executor for unknown type"
 jobstartpattern = re.compile(r"job start at (\w+ \w+ \d+ \d\d:\d\d:\d\d \w+ 20\d\d)")
 ppstartpattern  = re.compile(r"Pre-select \d+ entries out of \d+")
 donepattern     = re.compile(r"Complete at *(\w+ \w+ \d+ \d\d:\d\d:\d\d \w+ 20\d\d)")
-chunkpattern    = re.compile(r"Going to execute python job.py .* -n (\d+) .*")
+jcmdpattern     = re.compile(r"Going to execute python job.py (.*)")
+infilespattern  = re.compile(r"-i *(root\:[^ ]+\.root)") #[\w\/\-\:\.\,]+
+outdirpattern   = re.compile(r"-o *([\w\/\-]+)")
+outfilepattern  = re.compile(r"output file *= *([^ ]+\.root)")
+outfilepattern2 = re.compile(r"TreeProducerCommon is called *([^ ]+\.root)")
+chunkpattern    = re.compile(r"-n *(\d+)")
+rootpattern     = re.compile(r"root://.*?/.*?\.root")
 
 
 
 class Job:
   
-  def __init__(self,logfile):
-    nevents   = -1
-    jobid     = -1
-    taskid    = -1
-    runtime   = -1
-    chunk     = -1
-    jobstart  = None
-    done      = None
-    queue     = None
-    node      = None
-    ppstart   = False # post-processor started running
-    running   = False
-    waiting   = False
-    stuck     = False
-    failed    = False
-    files     = [ ]
+  def __init__(self,*args):
     
     # JOB INFO
-    jobid, taskid = getJobID(logfile)
+    if len(args)==1:
+      logfile  = args[0]
+      jobid, taskid = getJobID(logfile)
+    else:
+      jobid, taskid = args[:2]
+      logfiles = glob.glob("output_20*/*/logs/*.o%d.%d"%(jobid,taskid))
+      logfile  = logfiles[0] if logfiles else ""
+    nevents    = -1
+    jobid      = -1
+    taskid     = -1
+    runtime    = -1
+    chunk      = -1
+    jobstart   = None
+    done       = None
+    queue      = None
+    node       = None
+    ppstart    = False # post-processor started running
+    running    = False
+    waiting    = False
+    stuck      = False
+    failed     = False
+    outfile    = None
+    outdir     = None
+    infiles    = [ ]
     
     # QSTAT
     process   = subprocess.Popen("qstat", stdout=subprocess.PIPE, shell=True)
@@ -98,44 +113,64 @@ class Job:
         break
     
     # READ FILE
-    with open(logfile) as file:
-      for line in file:
+    if logfile:
+      with open(logfile) as file:
+        for line in file:
         
-        match = jobstartpattern.search(line)
-        if match:
-          jobstart = datetime.strptime(match.group(1),'%a %b %d %X CET %Y')
-          continue
+          match = jobstartpattern.search(line)
+          if match:
+            jobstart = datetime.strptime(match.group(1),'%a %b %d %X CET %Y')
+            continue
         
-        match = chunkpattern.search(line)
-        if match:
-          chunk = int(match.group(1))
-          continue
+          match = jcmdpattern.search(line)
+          if match:
+            args = match.group(1)
+            match = chunkpattern.search(args)
+            if match:
+              chunk = int(match.group(1))
+            match = infilespattern.search(args)
+            if match:
+              infiles = match.group(1).split(',')
+            match = outdirpattern.search(args)
+            if match:
+              outdir = match.group(1)
+            continue
         
-        match = ppstartpattern.search(line)
-        if match:
-          ppstart = True
-          continue
+          match = ppstartpattern.search(line)
+          if match:
+            ppstart = True
+            continue
         
-        match = donepattern.search(line)
-        if match:
-          done = datetime.strptime(match.group(1),'%a %b %d %X CET %Y')
-          break
+          match = donepattern.search(line)
+          if match:
+            done = datetime.strptime(match.group(1),'%a %b %d %X CET %Y')
+            break
         
-        match = nodepattern.search(line)
-        if match:
-          node = match.group(1)
-          continue
+          match = nodepattern.search(line)
+          if match:
+            node = match.group(1)
+            continue
         
-        matches = rootpattern.findall(line)
-        if matches and not files:
-          files = matches
-          continue
+          match = outfilepattern.search(line)
+          if match and not outfile:
+            outfile = match.group(1)
+            continue
         
-        if "segmentation violation" in line.lower() or "file probably overwritten: stopping reporting error messages" in line:
-          #or "terminate called after throwing an instance of 'std::bad_alloc'" in line:
-          failed = True
-          done = False
-          break
+          match = outfilepattern2.search(line)
+          if match and not outfile:
+            outfile = match.group(1)
+            continue
+        
+          #matches = rootpattern.findall(line)
+          #if matches and not files:
+          #  files = matches
+          #  continue
+        
+          if "segmentation violation" in line.lower() or "file probably overwritten: stopping reporting error messages" in line:
+            #or "terminate called after throwing an instance of 'std::bad_alloc'" in line:
+            failed = True
+            done = False
+            break
     
     if jobstart:
       if done:
@@ -149,7 +184,9 @@ class Job:
           stuck = not failed
     
     self.logfile  = logfile
-    self.files    = files
+    self.infiles  = infiles
+    self.outfile  = outfile
+    self.outdir   = outdir
     self.chunk    = chunk
     self.runtime  = runtime
     self.start    = jobstart
@@ -169,7 +206,7 @@ class Job:
       #return self.taskid>ojob.taskid
       return self.chunk>ojob.chunk
     return self.jobid>ojob.jobid
-  
+    
   def __str__(self):
     return '%d.%d (%d)'%(self.jobid,self.taskid,self.chunk)
     
@@ -205,12 +242,46 @@ def average(jobs):
   return printTime(sum(runtimes)/len(runtimes))
   
 
+def getRunningJobs():
+    jobs      = [ ]
+    stuck     = [ ]
+    running   = [ ]
+    waiting   = [ ]
+    process   = subprocess.Popen("qstat", stdout=subprocess.PIPE, shell=True)
+    (out,err) = process.communicate()
+    status    = process.wait()
+    for line in out.split('\n'):
+      match = qstatpattern.match(line)
+      if match:
+        if '-' in match.group(3):
+          #match2 = re.search(r"(\d+)-(\d+)",match.group(3))
+          #cmin, cmax = match2.group(1), match2.group(2)
+          continue
+        else:
+          jobid, taskid = int(match.group(1)), int(match.group(3))
+          job = Job(jobid,taskid)
+          jobs.append(job)
+          if job.waiting:
+            running.append(job)
+          if job.running:
+            running.append(job)
+          if job.stuck:
+            stuck.append(job)
+    print ">>>  waiting: %4d /%4d"%(len(waiting),len(jobs))
+    print ">>>  stuck:   %4d /%4d"%(len(stuck),len(jobs))
+    print ">>>  running: %4d /%4d"%(len(running),len(jobs))
+    #print ">>>  running: %4d /%4d, %12s"%(len(running),len(jobs),average(running))
+
 
 def main(args):
   
   years      = args.years
   channels   = args.channels
   njobs      = args.njobs
+  
+  if args.running:
+    getRunningJobs()
+    return
   
   for year in years:
     indir    = "output_%s/"%(year)
@@ -286,6 +357,7 @@ def main(args):
           print ">>>     done:    %4d /%4d, %12s"%(len(done[jobid]),ntot,average(done[jobid]) if done[jobid] else "") #+ ', '.join([str(j) for j in done[jobid]])
         
         print ">>>"
+    os.chdir('..')
 
 
 
